126a73457ed5ab5685d9cfe2a1409a6976295935
Alberto Lerda <albertolerda97@gmail.com>
2022-11-11 16:28:31 +0100

    change default socket bind

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-nohup ganache --chain.chainId $1 --wallet.seed "$2" &
+nohup ganache --server.host "0.0.0.0" --server.port 9485  --chain.chainId $1 --wallet.seed "$2" &
 
 truffle migrate
